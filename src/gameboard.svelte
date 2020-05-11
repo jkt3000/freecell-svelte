757d919e816fa269fa075d8af8b400a8a5f841ec
John Tajima <manjiro@gmail.com>
2020-05-11 11:41:55 -0400

    simplfy code into one obj

--- src/gameboard.svelte
+++ src/gameboard.svelte
@@ -5,15 +5,29 @@
   import GameCell from './gamecell.svelte';
   import Tableau from './tableau.svelte';
 
-  /* logic stuff */
-  import { columns } from './stores.js'; /* data store */
-  import Deck from './deck.js';
-  const HOMECELL_OFFSET = 8;
-  const FREECELL_OFFSET = 12;
+  import {columns} from './stores.js'; /* data store */
   
   export let gameId = parseInt(Math.random() * 1000000);
 
-  let Game = {
+  
+  const Game = {
+    HOMECELL_OFFSET: 8,
+    FREECELL_OFFSET: 12,
+    PLAYING_CARDS: [
+    "Ac", "Ad", "Ah", "As", 
+    "2c", "2d", "2h", "2s", 
+    "3c", "3d", "3h", "3s", 
+    "4c", "4d", "4h", "4s", 
+    "5c", "5d", "5h", "5s", 
+    "6c", "6d", "6h", "6s", 
+    "7c", "7d", "7h", "7s", 
+    "8c", "8d", "8h", "8s", 
+    "9c", "9d", "9h", "9s", 
+    "Tc", "Td", "Th", "Ts", 
+    "Jc", "Jd", "Jh", "Js", 
+    "Qc", "Qd", "Qh", "Qs", 
+    "Kc", "Kd", "Kh", "Ks" 
+    ],
     ranks: ['A','2','3','4','5','6','7','8','9','T','J','Q','K'],
     cardRank(card) { return card[0] },
     cardSuit(card) { return card[1] },
@@ -47,10 +61,10 @@
              (this.alternateColors(parent, child));
     },
     findEmptyHomeCell() {
-      return this.findEmptyCell(HOMECELL_OFFSET);
+      return this.findEmptyCell(this.HOMECELL_OFFSET);
     },
     findEmptyFreeCell() {
-      return this.findEmptyCell(FREECELL_OFFSET);
+      return this.findEmptyCell(this.FREECELL_OFFSET);
     },
     findEmptyCell(offset) {
       for (let i=0; i < 4; i++) {
@@ -59,7 +73,7 @@
       }
     },
     numEmptyFreeCells() {
-      let freecells = $columns.slice(FREECELL_OFFSET);
+      let freecells = $columns.slice(this.FREECELL_OFFSET);
       return freecells.filter(c => { return (c.length === 0); }).length;
     },
 
@@ -72,8 +86,58 @@
         let parent = [...$columns[i]].slice(-1).pop();
         if (Game.validParent(parent, card)) return i;
       }
+    },
+    Deck: class {
+      // generates a new random deck based on unique game_id
+      constructor(seed) {
+        this.cards = [];
+        this.seed = parseInt(seed,10) || parseInt((Math.random() * 1000000), 10);
+        this.shuffle();
+      }
+
+      pop() {
+        if (this.empty) {
+          return;
+        } else {
+          return this.cards.pop();
+        }
+      }
+
+      shuffle() {
+        if (!this.empty) { return; }
+        let deck = Array.from(Game.PLAYING_CARDS, function(id) { return id });
+        let state = this.seed;
+        for (let len = 52; len >= 2; len--) {
+          state = ((214013 * state) + 2531011) & 0x7FFFFFFF;
+          let index = (state >> 16) % len;
+          let last = len - 1;
+          [deck[index], deck[last]] = [deck[last], deck[index]];
+        }
+        this.cards = deck.reverse();
+      }
+
+      toTableau() {
+        let columns = 8;
+        let tableaus = [ 
+          new Array(), new Array(), new Array(), new Array(),
+          new Array(), new Array(), new Array(), new Array()
+        ];
+        let i = 0;
+        while (!this.empty) {
+          tableaus[i++ % columns].unshift(this.pop());
+        }
+        return tableaus;
+      }
+
+      get remaining() {
+        return this.cards.length;
+      }
+
+      get empty() {
+        return this.cards.length == 0;
+      }
     }
-  }
+  };
 
   /* returns array of selected cards for dragging */
   function selectCards(card, index) {
@@ -241,7 +305,7 @@
   });
 
   /* start up a new game */
-  let deck = new Deck(gameId);
+  let deck = new Game.Deck(gameId);
   let cards = deck.toTableau();
   $columns = cards.concat([[],[],[],[],[],[],[],[]]);
 </script>
@@ -253,12 +317,12 @@
     <div class='row headboard'>    
       <div class='cells-board homecells'>
         {#each Array(4) as _, index}
-          <GameCell cards={$columns[index + HOMECELL_OFFSET]} index={index + HOMECELL_OFFSET}  type='homecell' draggable={false}/>
+          <GameCell cards={$columns[index + Game.HOMECELL_OFFSET]} index={index + Game.HOMECELL_OFFSET}  type='homecell' draggable={false}/>
         {/each}
       </div>
       <div class='cells-board freecells'>
         {#each Array(4) as _, index}
-          <GameCell cards={$columns[index + FREECELL_OFFSET]} index={index + FREECELL_OFFSET}  type='freecell'/>
+          <GameCell cards={$columns[index + Game.FREECELL_OFFSET]} index={index + Game.FREECELL_OFFSET}  type='freecell'/>
         {/each}
       </div>
     </div>
