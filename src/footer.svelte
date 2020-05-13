848b237d5d5177edfb2e8509f6c149b9ed73dbaf
John Tajima <manjiro@gmail.com>
2020-05-13 12:04:52 -0400

    add titles to links

--- src/footer.svelte
+++ src/footer.svelte
@@ -30,27 +30,27 @@
 <nav class="navbar fixed-bottom navbar-dark bg-dark p-0" id='footer'>
   <ul class="nav justify-content-around" style='width:100%'>
     <li class="nav-item">
-      <a class="nav-link btn btn-link text-white" href="#" class:disabled={disableUndo} on:click|preventDefault={undo}>
+      <a class="nav-link btn btn-link text-white" class:disabled={disableUndo} on:click|preventDefault={undo} title="Undo last move">
         <i class="fas fa-backward"></i>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link btn btn-link text-white" href="#" on:click|preventDefault={newGame}>
+      <a class="nav-link btn btn-link text-white" on:click|preventDefault={newGame} title='New Game'>
         <i class="fas fa-power-off"></i>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link btn btn-link text-white" href="#" on:click|preventDefault={hint}>
+      <a class="nav-link btn btn-link text-white" on:click|preventDefault={hint} title="Get Hint (not implemented)">
         <i class="far fa-lightbulb"></i>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link btn btn-link text-white" href="#" on:click|preventDefault={restart}>
+      <a class="nav-link btn btn-link text-white" on:click|preventDefault={restart} title="Restart this game">
         <i class="far fa-undo"></i>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link btn btn-link text-white" href="#" on:click|preventDefault={settings}>
+      <a class="nav-link btn btn-link text-white" on:click|preventDefault={settings} title="Settings (not implemented)">
         <i class="fas fa-cog"></i>
       </a>
     </li>
